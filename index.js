93875afcf812976fcc726fd986d1070d776a0328
Mohsvitry94 <38892510+Mohsvitry94@users.noreply.github.com>
2018-05-10 21:59:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -24,23 +24,12 @@ Bot.on('message', message => {
       .addField("/cree", "pour voir la date de creatation du bot./ plus operationnel", )
       .addField("/createur", "pour voir le createur du bot./ plus operationnel", )
       .addField("/grade", "grade de la faction", )
-    .addField("/serverinfo", "info serveur", )
-    .addField("/r1 à /r9", "description des grade faction", )
+      .addField("/r1 à /r9", "description des grade faction", )
       .setFooter("edite by Mohsvitry")
     message.channel.sendEmbed(embed);
      console.log("commande aide demandée !");
   }
 
-    if (command === 'serverinfo') {
-    const embed = new Discord.MessageEmbed()
-      .setDescription(`${message.guild.name}'s Information and Details`)
-      .setThumbnail(message.guild.iconURL())
-      .setFooter(message.guild.owner.user.tag, message.guild.owner.user.avatarURL())
-      .addField('Members', `${message.guild.members.filter(member => member.user.bot).size} bots of ${message.guild.memberCount} members.`)
-      .addField('Channels', `${message.guild.channels.filter(chan => chan.type === 'voice').size} voice / ${message.guild.channels.filter(chan => chan.type === 'text').size} text`)
-      .addField('Roles', message.guild.roles.map(role => role.name).join(', '));
-    message.channel.send({ embed });
-  }
 };
   
     if (message.content === prefix + "grade") {
