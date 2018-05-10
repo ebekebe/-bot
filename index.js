693623ff4cded1fb4a24435ac4ae61faab851dd2
Mohsvitry94 <38892510+Mohsvitry94@users.noreply.github.com>
2018-05-10 21:17:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -63,5 +63,133 @@ Bot.on('message', message => {
     message.channel.send({ embed });
     console.log("commande r1 !");
   }
+if (message.content === prefix + "r2") {
+    const embed = new Discord.RichEmbed()
+      .setTitle("Sovietnik/Совиетник")
+      .setAuthor("Dmitri", "https://i.imgur.com/hdWynbs.jpg")
+      .setDescription("Bras droit du chef")
+      .setColor(0x00AE86)
+      .setThumbnail("https://i.imgur.com/hdWynbs.jpg")
+      .setFooter("Кховтсиновскаыа ", "https://i.imgur.com/hdWynbs.jpg", )
+      .addField("Sovietnik/Совиетник : ", "Le conseiller du Pakhan.")
+      .addField("test")
+     
+    message.channel.send({ embed });
+    console.log("commande r2 !");
+  }
 
+  if (message.content === prefix + "r3") {
+    const embed = new Discord.RichEmbed()
+      .setTitle("Obshchak/Общак")
+      .setAuthor("Dmitri", "https://i.imgur.com/hdWynbs.jpg")
+      .setDescription("trésorier")
+      .setColor(0x00AE86)
+      .setThumbnail("https://i.imgur.com/hdWynbs.jpg")
+      .setFooter("Кховтсиновскаыа ", "https://i.imgur.com/hdWynbs.jpg", )
+      .addField("Obshchak/Общак :", "Le trésorier et le chef bookmarker.")
+      .addField("test")
+     
+    message.channel.send({ embed });
+    console.log("commande r3 !");
+  }
+
+  if (message.content === prefix + "r4") {
+    const embed = new Discord.RichEmbed()
+      .setTitle("Torpedo/Торпедо")
+      .setAuthor("Dmitri", "https://i.imgur.com/hdWynbs.jpg")
+      .setDescription("Un tueur à gages")
+      .setColor(0x00AE86)
+      .setThumbnail("https://i.imgur.com/hdWynbs.jpg")
+      .setFooter("Кховтсиновскаыа ", "https://i.imgur.com/hdWynbs.jpg", )
+      .addField("Torpedo/Торпедо : ", "Un tueur à gages pour la famille ou le gang")
+      .addField("test")
+
+    message.channel.send({ embed });
+    console.log("commande r4 !");
+  }
+
+
+  if (message.content === prefix + "r5") {
+    const embed = new Discord.RichEmbed()
+      .setTitle("Brigadier/Бригадиер")
+      .setAuthor("Dmitri", "https://i.imgur.com/hdWynbs.jpg")
+      .setDescription("Un chef de groupe")
+      .setColor(0x00AE86)
+      .setThumbnail("https://i.imgur.com/hdWynbs.jpg")
+      .setFooter("Кховтсиновскаыа ", "https://i.imgur.com/hdWynbs.jpg", )
+      .addField("Brigadier/Бригадиер : ", "Un chef d'un groupe de membres inférieurs de la famille ou d'un gang.")
+      .addField("test")
+
+    message.channel.send({ embed });
+    console.log("commande r5 !");
+  }
+
+
+  if (message.content === prefix + "r6") {
+    const embed = new Discord.RichEmbed()
+      .setTitle("Boyevik/Боыевик ")
+      .setAuthor("Dmitri", "https://i.imgur.com/hdWynbs.jpg")
+      .setDescription("Un membre de la famille")
+      .setColor(0x00AE86)
+      .setThumbnail("https://i.imgur.com/hdWynbs.jpg")
+      .setFooter("Кховтсиновскаыа ", "https://i.imgur.com/hdWynbs.jpg", )
+      .addField("Boyevik/Боыевик : ", "Un membre de la famille ou du gang qui organise des crimes et fait des rackets.")
+      .addField("test")
+
+    message.channel.send({ embed });
+    console.log("commande r6 !");
+  }
+
+
+  if (message.content === prefix + "r7") {
+    const embed = new Discord.RichEmbed()
+      .setTitle("Kryshas/Крышас")
+      .setAuthor("Dmitri", "https://i.imgur.com/hdWynbs.jpg")
+      .setDescription("collecteur de dettes")
+      .setColor(0x00AE86)
+      .setThumbnail("https://i.imgur.com/hdWynbs.jpg")
+      .setFooter("Кховтсиновскаыа ", "https://i.imgur.com/hdWynbs.jpg", )
+      .addField("Kryshas/Крышас : ", "Un exécuteur et un collecteur de dettes. ",)
+      .addField("test")
+
+    message.channel.send({ embed });
+    console.log("commande r7 !");
+  }
+
+
+
+  if (message.content === prefix + "r8") {
+    const embed = new Discord.RichEmbed()
+      .setTitle("Byki/Быки")
+      .setAuthor("Dmitri", "https://i.imgur.com/hdWynbs.jpg")
+      .setDescription("garde du corps")
+      .setColor(0x00AE86)
+      .setThumbnail("https://i.imgur.com/hdWynbs.jpg")
+      .setFooter("Кховтсиновскаыа ", "https://i.imgur.com/hdWynbs.jpg", )
+      .addField("Byki/Быки :", "Un garde du corps pour les membres supérieurs de la famille ou du gang.")
+      .addField("test")
+
+    message.channel.send({ embed });
+    console.log("commande r8 !");
+  }
+
+
+  if (message.content === prefix + "r9") {
+    const embed = new Discord.RichEmbed()
+      .setTitle("Shestyorka/Схестыорка ")
+      .setAuthor("Dmitri", "https://i.imgur.com/hdWynbs.jpg")
+      .setDescription("associé")
+      .setColor(0x00AE86)
+      .setThumbnail("https://i.imgur.com/hdWynbs.jpg")
+      .setFooter("Кховтсиновскаыа ", "https://i.imgur.com/hdWynbs.jpg", )
+      .addField("Shestyorka/Схестыорка : ", "Un associé qui fait un travail pour la famille ou le gang mais qui n'est pas un membre à part entière.",)
+      .addField("test")
+
+    message.channel.send({ embed });
+    console.log("commande r9 !");
+  }
+
+
+
+  
 });
