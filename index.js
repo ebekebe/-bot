1cf5705d06a3e2dd143e89d1fda22b920726afea
Mohsvitry94 <38892510+Mohsvitry94@users.noreply.github.com>
2018-05-10 21:31:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -53,11 +53,11 @@ Bot.on('message', message => {
    if (message.content === prefix + "r1") {
     const embed = new Discord.RichEmbed()
       .setTitle("Pakhan/Пакхан")
-      .setAuthor("Dmitri", "https://i.imgur.com/hdWynbs.jpg")
+      .setAuthor("Dmitri", "")
       .setDescription("chef faction")
       .setColor(0x00AE86)
-      .setThumbnail("https://i.imgur.com/hdWynbs.jpg")
-      .setFooter("Кховтсиновскаыа ", "https://i.imgur.com/hdWynbs.jpg", )
+      .setThumbnail("https://i.imgur.com/7NY72fi.jpg")
+      .setFooter("Кховтсиновскаыа ", "", )
       .addField("Pakhan/Пакхан: ", "Chef d'une famille criminelle ou d'un gang russe.")
       .addField("test")
      
@@ -67,11 +67,11 @@ Bot.on('message', message => {
 if (message.content === prefix + "r2") {
     const embed = new Discord.RichEmbed()
       .setTitle("Sovietnik/Совиетник")
-      .setAuthor("Dmitri", "https://i.imgur.com/hdWynbs.jpg")
+      .setAuthor("Dmitri", "")
       .setDescription("Bras droit du chef")
       .setColor(0x00AE86)
-      .setThumbnail("https://i.imgur.com/hdWynbs.jpg")
-      .setFooter("Кховтсиновскаыа ", "https://i.imgur.com/hdWynbs.jpg", )
+      .setThumbnail("https://i.imgur.com/7NY72fi.jpg")
+      .setFooter("Кховтсиновскаыа ", "", )
       .addField("Sovietnik/Совиетник : ", "Le conseiller du Pakhan.")
       .addField("test")
      
@@ -82,11 +82,11 @@ if (message.content === prefix + "r2") {
   if (message.content === prefix + "r3") {
     const embed = new Discord.RichEmbed()
       .setTitle("Obshchak/Общак")
-      .setAuthor("Dmitri", "https://i.imgur.com/hdWynbs.jpg")
+      .setAuthor("Dmitri", "")
       .setDescription("trésorier")
       .setColor(0x00AE86)
-      .setThumbnail("https://i.imgur.com/hdWynbs.jpg")
-      .setFooter("Кховтсиновскаыа ", "https://i.imgur.com/hdWynbs.jpg", )
+      .setThumbnail("https://i.imgur.com/7NY72fi.jpg")
+      .setFooter("Кховтсиновскаыа ", "", )
       .addField("Obshchak/Общак :", "Le trésorier et le chef bookmarker.")
       .addField("test")
      
@@ -97,11 +97,11 @@ if (message.content === prefix + "r2") {
   if (message.content === prefix + "r4") {
     const embed = new Discord.RichEmbed()
       .setTitle("Torpedo/Торпедо")
-      .setAuthor("Dmitri", "https://i.imgur.com/hdWynbs.jpg")
+      .setAuthor("Dmitri", "")
       .setDescription("Un tueur à gages")
       .setColor(0x00AE86)
-      .setThumbnail("https://i.imgur.com/hdWynbs.jpg")
-      .setFooter("Кховтсиновскаыа ", "https://i.imgur.com/hdWynbs.jpg", )
+      .setThumbnail("https://i.imgur.com/7NY72fi.jpg")
+      .setFooter("Кховтсиновскаыа ", "", )
       .addField("Torpedo/Торпедо : ", "Un tueur à gages pour la famille ou le gang")
       .addField("test")
 
@@ -113,11 +113,11 @@ if (message.content === prefix + "r2") {
   if (message.content === prefix + "r5") {
     const embed = new Discord.RichEmbed()
       .setTitle("Brigadier/Бригадиер")
-      .setAuthor("Dmitri", "https://i.imgur.com/hdWynbs.jpg")
+      .setAuthor("Dmitri", "")
       .setDescription("Un chef de groupe")
       .setColor(0x00AE86)
-      .setThumbnail("https://i.imgur.com/hdWynbs.jpg")
-      .setFooter("Кховтсиновскаыа ", "https://i.imgur.com/hdWynbs.jpg", )
+      .setThumbnail("https://i.imgur.com/7NY72fi.jpg")
+      .setFooter("Кховтсиновскаыа ", "", )
       .addField("Brigadier/Бригадиер : ", "Un chef d'un groupe de membres inférieurs de la famille ou d'un gang.")
       .addField("test")
 
@@ -129,11 +129,11 @@ if (message.content === prefix + "r2") {
   if (message.content === prefix + "r6") {
     const embed = new Discord.RichEmbed()
       .setTitle("Boyevik/Боыевик ")
-      .setAuthor("Dmitri", "https://i.imgur.com/hdWynbs.jpg")
+      .setAuthor("Dmitri", "")
       .setDescription("Un membre de la famille")
       .setColor(0x00AE86)
-      .setThumbnail("https://i.imgur.com/hdWynbs.jpg")
-      .setFooter("Кховтсиновскаыа ", "https://i.imgur.com/hdWynbs.jpg", )
+      .setThumbnail("https://i.imgur.com/7NY72fi.jpg")
+      .setFooter("Кховтсиновскаыа ", "", )
       .addField("Boyevik/Боыевик : ", "Un membre de la famille ou du gang qui organise des crimes et fait des rackets.")
       .addField("test")
 
@@ -145,11 +145,11 @@ if (message.content === prefix + "r2") {
   if (message.content === prefix + "r7") {
     const embed = new Discord.RichEmbed()
       .setTitle("Kryshas/Крышас")
-      .setAuthor("Dmitri", "https://i.imgur.com/hdWynbs.jpg")
+      .setAuthor("Dmitri", "")
       .setDescription("collecteur de dettes")
       .setColor(0x00AE86)
-      .setThumbnail("https://i.imgur.com/hdWynbs.jpg")
-      .setFooter("Кховтсиновскаыа ", "https://i.imgur.com/hdWynbs.jpg", )
+      .setThumbnail("https://i.imgur.com/7NY72fi.jpg")
+      .setFooter("Кховтсиновскаыа ", "", )
       .addField("Kryshas/Крышас : ", "Un exécuteur et un collecteur de dettes. ",)
       .addField("test")
 
@@ -162,11 +162,11 @@ if (message.content === prefix + "r2") {
   if (message.content === prefix + "r8") {
     const embed = new Discord.RichEmbed()
       .setTitle("Byki/Быки")
-      .setAuthor("Dmitri", "https://i.imgur.com/hdWynbs.jpg")
+      .setAuthor("Dmitri", "")
       .setDescription("garde du corps")
       .setColor(0x00AE86)
-      .setThumbnail("https://i.imgur.com/hdWynbs.jpg")
-      .setFooter("Кховтсиновскаыа ", "https://i.imgur.com/hdWynbs.jpg", )
+      .setThumbnail("https://i.imgur.com/7NY72fi.jpg")
+      .setFooter("Кховтсиновскаыа ", "", )
       .addField("Byki/Быки :", "Un garde du corps pour les membres supérieurs de la famille ou du gang.")
       .addField("test")
 
@@ -178,11 +178,11 @@ if (message.content === prefix + "r2") {
   if (message.content === prefix + "r9") {
     const embed = new Discord.RichEmbed()
       .setTitle("Shestyorka/Схестыорка ")
-      .setAuthor("Dmitri", "https://i.imgur.com/hdWynbs.jpg")
+      .setAuthor("Dmitri", "")
       .setDescription("associé")
       .setColor(0x00AE86)
-      .setThumbnail("https://i.imgur.com/hdWynbs.jpg")
-      .setFooter("Кховтсиновскаыа ", "https://i.imgur.com/hdWynbs.jpg", )
+      .setThumbnail("https://i.imgur.com/7NY72fi.jpg")
+      .setFooter("Кховтсиновскаыа ", "", )
       .addField("Shestyorka/Схестыорка : ", "Un associé qui fait un travail pour la famille ou le gang mais qui n'est pas un membre à part entière.",)
       .addField("test")
 
